1de33bdbdaf14001b1f00780252fa3f302a698a3
tillber <33751240+tillber@users.noreply.github.com>
2020-01-27 16:10:01 +0100

    created applicant index page

--- src/main/java/se/madev/main/view/RequestDispatcher.java
+++ src/main/java/se/madev/main/view/RequestDispatcher.java
@@ -1,5 +1,10 @@
 package se.madev.main.view;
 
+import java.util.Collection;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +19,16 @@ public class RequestDispatcher {
 	
 	@GetMapping
 	String index() {
-		return "index";
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		Collection<GrantedAuthority> authorities = (Collection<GrantedAuthority>) authentication.getAuthorities();
+		
+		for (GrantedAuthority authority : authorities) {
+			if(authority.getAuthority().equals("RECRUITER")) {
+				return "recruiter/index";
+			}
+		}
+		
+		return "applicant/index";
 	}
 	
 	@GetMapping("/applicant")
