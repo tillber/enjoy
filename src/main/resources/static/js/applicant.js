3b3dd5aa2f7de3692a80269374af7b99ad7fa225
tillber <33751240+tillber@users.noreply.github.com>
2020-01-30 12:18:37 +0100

    modified html and js

--- src/main/resources/static/js/applicant.js
+++ src/main/resources/static/js/applicant.js
@@ -2,15 +2,15 @@ function deleteRow(element){
 	$(element).closest('tr').remove();
 }
 
-$( document ).ready(function() {
+$(document).ready(function() {
 	var exp_tbody = $("#exp-table > tbody");
 	var avail_tbody = $("#avail-table > tbody");
-	
+
 	$("#add-exp").click(function() {
 		var row = "<tr><td>" + $( "#exp-area option:selected" ).text() + "</td><td>" + $("#exp-years").val() + "</td><td><button onclick = deleteRow(this) class='button is-danger is-small'>Delete</button></td></tr>";
 		exp_tbody.append($(row));
 	});
-	
+
 	$("#add-avail").click(function() {
 		var row = "<tr><td>" + $("#avail-from").val() + "</td><td>" + $("#avail-to").val() + "</td><td><button onclick = deleteRow(this) class='button is-danger is-small'>Delete</button></td></tr>";
 		avail_tbody.append($(row));
