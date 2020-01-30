5d535ae4b3e32a9b02f39ee7efb4cd331267c4e3
Daria Galal <dgalal@kth.se>
2020-01-30 10:23:50 +0100

    login tests updated

--- src/test/java/se/madev/main/LoginTests.java
+++ src/test/java/se/madev/main/LoginTests.java
@@ -1,6 +1,9 @@
 package se.madev.main;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -8,26 +11,53 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
+
+/**
+ * Integration testing with login/logout
+ * @author Group 3
+ *
+ */
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class LoginTests {
 	
+	private final String LOGIN_ERROR = "/login?error"; //error page
+	private final String LOGIN_SUCCESS = ""; //default index page
+	
 	@Autowired
 	private MockMvc mvc;
 	
 	@Test
-	public void loginWithCorrectCredentialsApplicant() throws Exception {
-		RequestBuilder req = formLogin("/login").user("admin").password("root");
-		mvc.perform(req).andExpect(redirectedUrl("applicant/index")).andExpect(status().isFound());
+	public void loginWithTrueCredentials() throws Exception {
+		RequestBuilder req = formLogin("/login").user("admin").password("root"); 		//enter correct user credentials
+		mvc.perform(req).andExpect(redirectedUrl("/")) 									//expect to be redirected to index page after login
+		.andExpect(status().isFound()) 													//expect page to be found (Code 302)
+		.andExpect(content().string(containsString(LOGIN_SUCCESS))) 					//expect response body to contain empty string
+		.andExpect(authenticated().withUsername("admin")); 													//expect the user to be authenticated
 	}
 	
-//	@Test
-//	public void loginWithCorrectCredentialsRecruiter() throws Exception {
-//		RequestBuilder req = formLogin().user("USERNAME").password("PASSWORD");
-//		mvc.perform(req).andExpect(redirectedUrl("recruiter/index")).andExpect(status().isFound());
-//	}
+	@Test
+	public void loginWithFalseCredentials() throws Exception {
+		RequestBuilder req = formLogin("/login").user("badmin").password("broot"); 		//enter false user credentials
+		mvc.perform(req).andExpect(redirectedUrl(LOGIN_ERROR)) 							//expect to be redirected to error page
+		.andExpect(status().isFound()); 												//expect page to be found (COde 302)
+	}
+	
+	@Test 
+	public void LoginWithTrueCredentialsThenLogout() throws Exception {
+		RequestBuilder req = formLogin("/login").user("admin").password("root"); 		//enter correct user credentials
+		mvc.perform(req).andExpect(redirectedUrl("/")) 									//expect to be redirected to index page after login
+		.andExpect(status().isFound()) 													//expect page to be found (Code 302)
+		.andExpect(content().string(containsString(LOGIN_SUCCESS))) 					//expect response body to contain empty string
+		.andExpect(authenticated().withUsername("admin")); 
+		
+		mvc.perform(SecurityMockMvcRequestBuilders.logout("/logout"))
+		.andExpect(redirectedUrl("/login"))
+		.andExpect(content().string(containsString("")));
+	}
 	
 }
