ace81e940b93f7d8b44cb5e8db1cd66b168a489d
tillber <33751240+tillber@users.noreply.github.com>
2020-02-05 09:58:36 +0100

    updated User object

--- src/main/java/se/madev/main/model/User.java
+++ src/main/java/se/madev/main/model/User.java
@@ -1,33 +1,103 @@
 package se.madev.main.model;
 
 import java.io.Serializable;
-
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name="users")
 public class User implements Serializable{
 
     @Id
-    @Column(name="userid")
+    @Column(name="id")
     private int id;
+    
+    @Column(name="firstName")
+    private String firstName;
+    
+    @Column(name="lastName")
+    private String lastName;
+    
+    @Column(name="email")
+    private String email;
 
     @Column(name="username")
     private String username;
 
     @Column(name="password")
     private String password;
+    
+    @Column(name="dateOfBirth")
+    private Date dateOfBirth;
+    
+    @Column(name="role")
+    private int role;
 
-    public String getUsername(){
-        return this.username;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public String getPassword(){
-        return this.password;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public int getRole() {
+		return role;
+	}
+
+	public void setRole(int role) {
+		this.role = role;
+	}
 }
