c01c1e5bf97ef16e959696723f328f1ee742757b
Daria Galal <dgalal@kth.se>
2020-02-05 14:49:18 +0100

    Merge changes

--- src/test/java/se/madev/main/DatabaseTest.java
+++ src/test/java/se/madev/main/DatabaseTest.java
@@ -3,7 +3,6 @@ package se.madev.main;
 import static org.assertj.core.api.Assertions.assertThat;
 import java.sql.Date;
 
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import se.madev.main.integration.UserRepository;
-import se.madev.main.model.MyUserDetails;
 import se.madev.main.model.User;
 
 @RunWith(SpringRunner.class)
