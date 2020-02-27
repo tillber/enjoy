87ae9800d923fae63977bebcee8a5e419dd4018f
Daria Galal <dgalal@kth.se>
2020-02-27 17:12:22 +0100

    fixed an issue with date of birth validation

--- src/main/java/se/madev/main/model/UnderagedValidator.java
+++ src/main/java/se/madev/main/model/UnderagedValidator.java
@@ -1,5 +1,9 @@
 package se.madev.main.model;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 
 import javax.validation.ConstraintValidator;
@@ -7,12 +11,22 @@ import javax.validation.ConstraintValidatorContext;
 
 public class UnderagedValidator implements ConstraintValidator<NonUnderAged, Date>{
 	
-	private final Date CURRENT_DATE = new Date();
 	private final int MIN_AGE = 15;
+	private final Date CURRENT_DATE = new Date();
+	
+	private LocalDate convertToLocalDate(Date date) {
+		Instant instant = Instant.ofEpochMilli(date.getTime());
+		LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+		LocalDate localDate = localDateTime.toLocalDate();
+		
+		return localDate;
+	}
 
 	@Override
 	public boolean isValid(Date value, ConstraintValidatorContext context) {
-		return (CURRENT_DATE.compareTo(value) >= MIN_AGE);
+		LocalDate today = convertToLocalDate(CURRENT_DATE);
+		LocalDate userBirthDate = convertToLocalDate(value);
+		return (today.getYear() - userBirthDate.getYear() >= MIN_AGE);
 	}
 
 }
