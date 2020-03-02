34b0fa9fc8405a9e819cdfd4c1320bac85feeb3c
tillber <33751240+tillber@users.noreply.github.com>
2020-03-02 08:38:47 +0100

    added extensive javadoc comments to User object

--- src/main/java/se/madev/main/model/User.java
+++ src/main/java/se/madev/main/model/User.java
@@ -27,44 +27,72 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Table(name="users")
 public class User implements Serializable{
 
+	/**
+	 * The user's id.
+	 */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
     private int id;
     
+    /**
+     * The first name of the user.
+     */
     @Column(name="firstname")
     @NotBlank(message = "First name is mandatory")
     @Pattern(regexp = "^[A-Za-z-]*$",message = "Invalid first name!")
     private String firstName;
     
+    /**
+     * The last name of the user.
+     */
     @Column(name="lastname")
     @NotBlank(message = "Last name is mandatory")
     @Pattern(regexp = "^[A-Za-z-]*$", message = "Invalid last name!")
     private String lastName;
     
+    /**
+     * The user's email address.
+     */
     @Column(name="email", unique = true)
     @NotBlank(message = "Email is mandatory")
     @Email(message = "Email should be valid")
     private String email;
 
+    /**
+     * The username of the user.
+     */
     @Column(name="username", unique = true)
     @NotBlank(message = "Username is mandatory")
     private String username;
 
+    /**
+     * The user's password.
+     */
     @Column(name="password")
     @NotBlank(message = "Password is mandatory")
     private String password;
     
+    /**
+     * The birth date of the user.
+     */
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Past(message = "Invalid date of birth!")
     @NonUnderAged(message = "You must be 15 or older to register!")
     @Column(name="dateofbirth")
     private Date dateOfBirth;
     
+    /**
+     * The role of the user.
+     */
     @ManyToOne
     @JoinColumn(name = "role")
     private Role role;
     
+    /**
+     * Creates a new User using the given MyUserDetails object.
+     * @param user The MyUserDetails object to depict.
+     */
     public User(MyUserDetails user) {
     	this.id = user.getId();
     	this.username = user.getUsername();
